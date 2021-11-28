8d71cf414b5b0014941ddc6569c01db3dbd084ee
nachope1201 <nachoapereyra@gmail.com>
2021-11-28 02:07:20 -0300

    Agregué el modelo de criminal con un paquete que contiene sus posibles sexos.

--- src/main/java/edu/fiuba/algo3/modelo/Personas/Criminal.java
+++ src/main/java/edu/fiuba/algo3/modelo/Personas/Criminal.java
@@ -1,4 +1,14 @@
 package edu.fiuba.algo3.modelo.Personas;
 
+import edu.fiuba.algo3.modelo.Sexo.*;
 public class Criminal {
+    Sexo sexo;
+
+    public Criminal(){
+        sexo = new SexoFemenino();
+    }
+
+    public void robar(){
+    }
+
 }
