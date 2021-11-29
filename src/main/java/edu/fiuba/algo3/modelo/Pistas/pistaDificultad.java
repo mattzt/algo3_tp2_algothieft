abea2e2c557e9aa46f65311b7d30fae90298c455
gonzashima <gonza.shima@gmail.com>
2021-11-29 18:55:29 -0300

    detalles menores

--- src/main/java/edu/fiuba/algo3/modelo/Pistas/pistaDificultad.java
+++ src/main/java/edu/fiuba/algo3/modelo/Pistas/pistaDificultad.java
@@ -10,25 +10,25 @@ public class pistaDificultad {
     public ArrayList<String> obtenerPistasDeDificultad(int arrestos){
         String rutas;
         if(arrestos < 5){
-            rutas = "C:\\Users\\Agustin\\IdeaProjects\\algo3_tp2_algothieft\\src\\main\\java\\edu\\fiuba\\algo3\\modelo\\Resources\\PistasFaciles.txt"
+            rutas = "C:\\Users\\Agustin\\IdeaProjects\\algo3_tp2_algothieft\\src\\main\\java\\edu\\fiuba\\algo3\\modelo\\Resources\\PistasFaciles.txt";
         }
-        if(arrestos < 15 && arrestos >= 5){
-            rutas = "C:\\Users\\Agustin\\IdeaProjects\\algo3_tp2_algothieft\\src\\main\\java\\edu\\fiuba\\algo3\\modelo\\Resources\\PistasMedios.txt"
+        else if(arrestos < 15){
+            rutas = "C:\\Users\\Agustin\\IdeaProjects\\algo3_tp2_algothieft\\src\\main\\java\\edu\\fiuba\\algo3\\modelo\\Resources\\PistasMedios.txt";
         }
-        if(arrestos >= 15){
-            rutas = "C:\\Users\\Agustin\\IdeaProjects\\algo3_tp2_algothieft\\src\\main\\java\\edu\\fiuba\\algo3\\modelo\\Resources\\PistasDificiles.txt"
+        else{
+            rutas = "C:\\Users\\Agustin\\IdeaProjects\\algo3_tp2_algothieft\\src\\main\\java\\edu\\fiuba\\algo3\\modelo\\Resources\\PistasDificiles.txt";
         }
         return cargarPistasDificultad(rutas);
     }
 
     private ArrayList<String> cargarPistasDificultad(String ruta){
-        ArrayList<String> pistas;
+        ArrayList<String> pistas = new ArrayList<>();
         File archivo = new File (ruta);
         FileReader fr = new FileReader (archivo);
         BufferedReader br = new BufferedReader(fr);
         String linea;
         while((linea=br.readLine())!=null){
-            pistas.add(linea)
+            pistas.add(linea);
         }
         return pistas;
     }
