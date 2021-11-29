78aba7cbc2780cbaf36a72638ab2f417272a6a58
Gonzalo <gonza.shima@gmail.com>
2021-11-29 18:44:14 -0300

    detalles menores

--- src/main/test/edu/fiuba/algo3/modelo/PidoPistaFacilBanco.java
+++ src/main/test/edu/fiuba/algo3/modelo/PidoPistaFacilBanco.java
@@ -5,16 +5,18 @@ import edu.fiuba.algo3.modelo.Personas.Novato;
 import edu.fiuba.algo3.modelo.Pistas.Pista;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class PidoPistaFacilBanco {
 
     @Test
-    public class pidoPistaFacil{
-        Novato novato = Novato new();
-        Banco banco = Banco new();
-        Pista pista = Pista new();
+    public void pidoPistaFacil(){
+        Novato novato = new Novato();
+        Banco banco = new Banco();
+        Pista pista = new Pista();
         String resultado = pista.buscarPistaPara(banco, novato);
         String parcial = "Dijo que queria cambiar su dinero por Rublos";
-        assertTrue(resultado==parcial);
+        assertTrue(resultado == parcial);
 
     }
 }
