9bb5a8a5e80028ac1af83012380ea634460ca164
gonzashima <gonza.shima@gmail.com>
2021-12-14 13:45:50 -0300

    se cambiaron los test de las caracteristicas para no comparar con strings

--- src/main/test/edu/fiuba/algo3/modelo/HobbiesTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/HobbiesTest.java
@@ -3,43 +3,43 @@ package edu.fiuba.algo3.modelo;
 import edu.fiuba.algo3.modelo.Criminales.Hobbies.*;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HobbiesTest {
 
     @Test
     public void alpinismoTest(){
         Alpinismo alpinismo = new Alpinismo();
-        assertEquals("Alpinismo", alpinismo.valor());
+        assertTrue(alpinismo.equals(new Alpinismo()));
     }
 
     @Test
     public void croquetTest(){
         Croquet croquet = new Croquet();
-        assertEquals("Croquet", croquet.valor());
+        assertTrue(croquet.equals(new Croquet()));
     }
 
     @Test
     public void musicaTest(){
         Musica musica = new Musica();
-        assertEquals("Musica", musica.valor());
+        assertTrue(musica.equals(new Musica()));
     }
 
     @Test
     public void natacionTest(){
         Natacion natacion = new Natacion();
-        assertEquals("Natacion", natacion.valor());
+        assertTrue(natacion.equals(new Natacion()));
     }
 
     @Test
     public void paracaidismoTest(){
         Paracaidismo paracaidismo = new Paracaidismo();
-        assertEquals("Paracaidismo", paracaidismo.valor());
+        assertTrue(paracaidismo.equals(new Paracaidismo()));
     }
 
     @Test
     public void tenisTest(){
         Tenis tenis = new Tenis();
-        assertEquals("Tenis", tenis.valor());
+        assertTrue(tenis.equals(new Alpinismo()));
     }
 }
