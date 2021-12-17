836039dd1609b504b8f0c99dafe98ddabca02247
Agustin <aguselgonza@gmail.com>
2021-12-17 19:05:15 -0300

    javafx

--- src/main/java/edu/fiuba/algo3/Main.java
+++ src/main/java/edu/fiuba/algo3/Main.java
@@ -1,7 +1,23 @@
 package edu.fiuba.algo3;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class Main {
-    public static void main(String[] args) {
-        App.main(args);
+public class Main extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception{
+
+        var label = new Label("Hola");
+        var scene = new Scene(new StackPane(label), 800, 800);
+        stage.setScene(scene);
+        stage.show();
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
