0065dab7f6e474f7db5c816c4f96d0dd63704202
gonzashima <gonza.shima@gmail.com>
2021-12-19 20:34:00 -0300

    pantalla de la computadora

--- src/main/java/edu/fiuba/algo3/Interfaz/ControladorComputadora.java
+++ src/main/java/edu/fiuba/algo3/Interfaz/ControladorComputadora.java
@@ -1,10 +1,12 @@
 package edu.fiuba.algo3.Interfaz;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.RadioButton;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -12,6 +14,21 @@ import java.io.IOException;
 import java.net.URL;
 
 public class ControladorComputadora {
+
+    @FXML
+    private RadioButton masculino, femenino;
+
+    @FXML
+    private RadioButton anillo, tatuaje, cicatriz, joyas;
+
+    @FXML
+    private RadioButton alpinismo, croquet, musica, natacion, paracaidismo, tenis;
+
+    @FXML
+    private RadioButton castanio, negro, rojo, rubio;
+
+    @FXML
+    private RadioButton deportivo, descapotable, moto, limusina;
 
     public void irAMenu(ActionEvent event) throws IOException {
         URL url = new File("src/main/java/edu/fiuba/algo3/Interfaz/MenuPrincipal.fxml").toURI().toURL();
