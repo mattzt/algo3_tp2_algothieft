f864cd7d61a51fd0f3907269569145e9f57e4fde
mattzt <montero987@gmail.com>
2021-12-20 06:14:46 -0300

    [UI] -> Input compatible con enter.

--- src/main/java/edu/fiuba/algo3/Interfaz/Controller/ControladorPantallaInicial.java
+++ src/main/java/edu/fiuba/algo3/Interfaz/Controller/ControladorPantallaInicial.java
@@ -1,10 +1,14 @@
 package edu.fiuba.algo3.Interfaz.Controller;
 
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -14,15 +18,26 @@ import java.net.URL;
 
 public class ControladorPantallaInicial extends Controlador {
 
-    public void logearse(ActionEvent event) throws IOException {
-        URL url = new File("src/main/java/edu/fiuba/algo3/Interfaz/Views/MenuPrincipal.fxml").toURI().toURL();
-        AnchorPane root = FXMLLoader.load(url);
+    public Button loguearse;
+    public TextField unNombre;
 
-        Stage stage = (Stage) (((Node) event.getSource()).getScene().getWindow());
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.centerOnScreen();
-        stage.show();
+    public void clickLogIn(ActionEvent event) throws IOException {
+        logIn(event);
     }
+
+    public void enterKey(KeyEvent keyEvent) throws IOException {
+        if (keyEvent.getCode() == KeyCode.ENTER) logIn(keyEvent);
+    }
+
+    private void logIn(Event unEvento) throws IOException {
+            URL url = new File("src/main/java/edu/fiuba/algo3/Interfaz/Views/MenuPrincipal.fxml").toURI().toURL();
+            AnchorPane root = FXMLLoader.load(url);
+
+            Stage stage = (Stage) (((Node) unEvento.getSource()).getScene().getWindow());
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.centerOnScreen();
+            stage.show();
+        }
 }
