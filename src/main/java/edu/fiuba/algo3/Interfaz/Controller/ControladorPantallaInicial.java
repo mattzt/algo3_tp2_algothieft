2e5cf2db66c1d86adfe19e6d8e1c18e097910c3f
mattzt <montero987@gmail.com>
2021-12-20 06:31:23 -0300

    [UI] -> Verifica que el input no sea vacio

--- src/main/java/edu/fiuba/algo3/Interfaz/Controller/ControladorPantallaInicial.java
+++ src/main/java/edu/fiuba/algo3/Interfaz/Controller/ControladorPantallaInicial.java
@@ -22,22 +22,30 @@ public class ControladorPantallaInicial extends Controlador {
     public TextField unNombre;
 
     public void clickLogIn(ActionEvent event) throws IOException {
-        logIn(event);
+        if (!nombreCorrecto(unNombre.getText())) logIn(event);
     }
 
     public void enterKey(KeyEvent keyEvent) throws IOException {
-        if (keyEvent.getCode() == KeyCode.ENTER) logIn(keyEvent);
+        if (!nombreCorrecto(unNombre.getText()) && keyEvent.getCode() == KeyCode.ENTER ){
+            logIn(keyEvent);
+            System.out.println("HOLA");
+            System.out.println(unNombre.getText());
+        }
     }
 
     private void logIn(Event unEvento) throws IOException {
-            URL url = new File("src/main/java/edu/fiuba/algo3/Interfaz/Views/MenuPrincipal.fxml").toURI().toURL();
-            AnchorPane root = FXMLLoader.load(url);
+        URL url = new File("src/main/java/edu/fiuba/algo3/Interfaz/Views/MenuPrincipal.fxml").toURI().toURL();
+        AnchorPane root = FXMLLoader.load(url);
 
-            Stage stage = (Stage) (((Node) unEvento.getSource()).getScene().getWindow());
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-            stage.setResizable(false);
-            stage.centerOnScreen();
-            stage.show();
-        }
+        Stage stage = (Stage) (((Node) unEvento.getSource()).getScene().getWindow());
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.centerOnScreen();
+        stage.show();
+    }
+
+    private boolean nombreCorrecto(String texto){
+        return texto.isBlank();
+    }
 }
