6ca1879addde7344ec02931b62b33e23673190b3
gonzashima <gonza.shima@gmail.com>
2022-01-01 19:39:51 -0300

    se avanzo con el estilo del menu principal

--- src/main/java/edu/fiuba/algo3/Interfaz/Views/EscenaMenuPrincipal.java
+++ src/main/java/edu/fiuba/algo3/Interfaz/Views/EscenaMenuPrincipal.java
@@ -9,19 +9,24 @@ import edu.fiuba.algo3.modelo.Policia.Policia;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.effect.BlendMode;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class EscenaMenuPrincipal extends AnchorPane {
 
-    Stage stage;
-    Policia policia;
-    Partida partida;
+    private final Stage stage;
+    private final Policia policia;
+    private final Partida partida;
 
     public EscenaMenuPrincipal(Stage stage) throws NoExisteError, IOException {
         partida = Partida.getInstance();
@@ -30,12 +35,13 @@ public class EscenaMenuPrincipal extends AnchorPane {
         this.setPrefHeight(700);
         this.setPrefWidth(900);
 
+        configurarEstilo();
         setBotones();
         setInfo();
         setObjetoRobado();
     }
 
-    private void setBotones(){
+    private void setBotones() throws MalformedURLException {
         HBox contenedorBotones = new HBox();
         contenedorBotones.setPrefHeight(100);
         contenedorBotones.setPrefWidth(550);
@@ -51,16 +57,52 @@ public class EscenaMenuPrincipal extends AnchorPane {
         botonViajar.setOnAction(new BotonEscenaViajarHandler(stage));
 
         botonCiudad.setPrefHeight(100);
-        botonCiudad.setPrefWidth(200);
+        botonCiudad.setPrefWidth(183);
         botonCiudad.setFont(Font.font(14));
 
         botonViajar.setPrefHeight(100);
-        botonViajar.setPrefWidth(200);
+        botonViajar.setPrefWidth(183);
         botonViajar.setFont(Font.font(14));
 
         botonComputadora.setPrefHeight(100);
-        botonComputadora.setPrefWidth(200);
+        botonComputadora.setPrefWidth(183);
         botonComputadora.setFont(Font.font(14));
+
+        URL css = new File("src/main/java/edu/fiuba/algo3/Interfaz/Views/resources/darkTheme.css").toURI().toURL();
+
+        botonCiudad.getStylesheets().add(String.valueOf(css));
+        botonViajar.getStylesheets().add(String.valueOf(css));
+        botonComputadora.getStylesheets().add(String.valueOf(css));
+
+        URL ciudad = new File("src/main/java/edu/fiuba/algo3/Interfaz/Views/resources/buttons/explore.png").toURI().toURL();
+        URL viajar = new File("src/main/java/edu/fiuba/algo3/Interfaz/Views/resources/buttons/viajar.png").toURI().toURL();
+        URL computadora = new File("src/main/java/edu/fiuba/algo3/Interfaz/Views/resources/buttons/interpol.png").toURI().toURL();
+
+        ImageView imagenCiudad = new ImageView(String.valueOf(ciudad));
+        ImageView imagenViajar = new ImageView(String.valueOf(viajar));
+        ImageView imagenComputadora = new ImageView(String.valueOf(computadora));
+
+        imagenCiudad.setFitWidth(100);
+        imagenCiudad.setFitHeight(100);
+        imagenCiudad.setPickOnBounds(true);
+        imagenCiudad.setBlendMode(BlendMode.HARD_LIGHT);
+        imagenCiudad.setPreserveRatio(true);
+
+        imagenViajar.setFitWidth(100);
+        imagenViajar.setFitHeight(100);
+        imagenViajar.setPickOnBounds(true);
+        imagenViajar.setBlendMode(BlendMode.HARD_LIGHT);
+        imagenViajar.setPreserveRatio(true);
+
+        imagenComputadora.setFitWidth(100);
+        imagenComputadora.setFitHeight(100);
+        imagenComputadora.setPickOnBounds(true);
+        imagenComputadora.setBlendMode(BlendMode.HARD_LIGHT);
+        imagenComputadora.setPreserveRatio(true);
+
+        botonCiudad.setGraphic(imagenCiudad);
+        botonViajar.setGraphic(imagenViajar);
+        botonComputadora.setGraphic(imagenComputadora);
 
         contenedorBotones.getChildren().addAll(botonCiudad, botonComputadora, botonViajar);
 
@@ -125,5 +167,24 @@ public class EscenaMenuPrincipal extends AnchorPane {
         contenedor.getChildren().add(mensaje);
 
         this.getChildren().add(contenedor);
+    }
+
+    private void configurarEstilo() throws MalformedURLException {
+        this.setStyle("-fx-background-color: grey;");
+
+
+        URL url = new File("src/main/java/edu/fiuba/algo3/Interfaz/Views/resources/background.jpg").toURI().toURL();
+        ImageView fondo = new ImageView(String.valueOf(url));
+        fondo.setBlendMode(BlendMode.MULTIPLY);
+        fondo.setFitHeight(566);
+        fondo.setFitWidth(900);
+        fondo.setLayoutY(-32);
+        fondo.setPickOnBounds(true);
+        fondo.setPreserveRatio(true);
+        fondo.setScaleY(1.26);
+        fondo.setStyle("-fx-translate-y: 100;");
+
+        this.getChildren().add(fondo);
+
     }
 }
